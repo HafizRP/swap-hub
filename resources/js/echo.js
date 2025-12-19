fc5678d042eaaba75eb40b996317e1ed400c9cd8
HafizRP23 <hafiz.tradersfamily@gmail.com>
2025-12-20 03:19:28 +0700

    chore: Hardcode Pusher app key and cluster values in Echo configuration.

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,8 +5,8 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    key: '4ac83a58a7171f24d51b',
+    cluster: 'ap1',
     forceTLS: true,
     encrypted: true,
 });
