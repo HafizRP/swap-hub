586e2e325fcfa05c7ebad339dcc490300d777990
HafizRP23 <hafiz.tradersfamily@gmail.com>
2026-01-05 15:33:21 +0700

    feat: Configure Laravel Echo with `VITE_BROADCAST_DRIVER` and simplified environment variable access in Vite.

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,15 +1,13 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
+    broadcaster: import.meta.env.VITE_BROADCAST_DRIVER,
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wssPort: import.meta.env.VITE_PUSHER_PORT,
+    forceTLS: import.meta.env.VITE_PUSHER_SCHEME,
     enabledTransports: ['ws', 'wss'],
 });
