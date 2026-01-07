e433c36f75fb574b5f98b270e599850c6e3d20e8
HafizRP23 <hafiz.tradersfamily@gmail.com>
2026-01-07 10:44:41 +0700

    config: Explicitly set Echo broadcaster to 'pusher' and add `wsHost` configuration.

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,9 +4,10 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: import.meta.env.VITE_BROADCAST_DRIVER,
+    broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
     wssPort: import.meta.env.VITE_PUSHER_PORT,
     forceTLS: import.meta.env.VITE_PUSHER_SCHEME,
     enabledTransports: ['ws', 'wss'],
